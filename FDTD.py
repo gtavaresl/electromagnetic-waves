99b14973c3cc9cc977b0e8c0a2d080eefdac142b
Gabriell Tavares <gabrielltavsb@gmail.com>
2020-05-20 13:06:42 -0300

    Update FDTD.py

--- FDTD.py
+++ FDTD.py
@@ -2,6 +2,7 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import argparse
 import time
 
 def u(t):
@@ -12,30 +13,51 @@ def Vs(t):
     return u(t)-u(t-((K*dz)/(10*Uf)))
     
 def update(n):
-    ln.set_ydata(v[n])
-    return ln,
+    line[0].set_ydata(i[n])
+    line[1].set_ydata(v[n])
+    return line
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-s","--source", default = int(0), help = "mode of source")
+ap.add_argument("-c","--charge", default = int(100), help = "impedancy of charge")
+ap.add_argument("-k", "--K", default = int(100), help = "number of iterations in z")
+args = vars(ap.parse_args())
 
 Uf = 2.7e8
 Z0 = 50
 Rs = 75
-Rl = 100
+Rl = int(args["charge"])
 
 L = Z0/Uf
 C = 1/(Z0*Uf)
 
 dz = 1e-3
-dt = 0.6*(dz/Uf)
-K = 2000
+dt = 0.8*(dz/Uf)
+K = int(args["K"])
 N = int((4*K*dz)/(Uf*dt)) 
 Total = (10*K*dz)/Uf
 
 v = np.zeros((N,K))
 i = np.zeros((N,K-1))
 
-z = np.array(range(K))*dz
+z = np.array(range(K-1))*dz
 
 b1 = (2*dt)/(Rs*C*dz)
-b2 = (2*dt)/(Rl*C*dz)
+
+flag_short = False
+#Open circuit
+b2 = 0
+flag_open = True
+if Rl == 0:
+    #Short circuit
+    b2 = math.inf
+    flag_short = True
+elif Rl > 0:
+    #Charge
+    b2 = (2*dt)/(Rl*C*dz)
+    flag_open = False
+
 r = (dt**2)/(L*C*(dz**2))
 
 ini = time.time()
@@ -52,12 +74,17 @@ for n in range(1, N):
     for k in range(1, K-1):
         v[n, k] = v[n-1, k] - (i[n-1, k] - i[n-1, k-1])
     #Charge border condition
-    v[n, K-1] = (1-b2)*v[n-1, K-1] + 2*i[n-1, K-2]
+    if flag_short:
+        v[n, K-1] = 0
+    else:
+        v[n, K-1] = (1-b2)*v[n-1, K-1]
+        if not flag_open:
+            v[n, K-1] += 2*i[n-1, K-2]
     #Current loop
     for k in range(K-1):
         i[n, k] = i[n-1, k] - r*(v[n, k+1] - v[n, k])
 
-v = (dt*v)/(C*dz)
+v *= dt/(C*dz)
 
 fim = time.time() - ini
 
@@ -74,11 +101,21 @@ print("Tempo de simulacao = ", Total)
 #print("Iinf = ", 1/(Rs+Rl))
 #print(i[N-1])
 
-fig, ax = plt.subplots()
-ln,   = ax.plot(z, v[0])
+fig, (ax1, ax2) = plt.subplots(2)
+line1,    = ax1.plot(z, i[0], color='r', label='Current [A]')
+ax1.set_ylim(-0.025, 0.025)
+ax1.grid(True)
 
-ani = FuncAnimation(fig, update, np.array(range(N)), interval = 1,blit = True)
 
-plt.grid(True)
-plt.ylim(-2,2)
+z = np.append(z, [(K-1)*dz])
+line2,    = ax2.plot(z, v[0], color='b', label='Voltage [V]')
+ax2.set_ylim(-1, 1)
+ax2.grid(True)
+
+
+line = (line1, line2)
+
+
+ani = FuncAnimation(fig, update, np.array(range(N)), interval = 20, blit = True)
+
 plt.show()
