ce709c12fef5c31ba9cb6ae09f1622a094c0c33f
Gabriell Tavares <35067170+gtavaresl@users.noreply.github.com>
2020-06-23 18:31:33 -0300

    Add files via upload

--- FDTD-wave/part_1.py
+++ FDTD-wave/part_1.py
@@ -7,10 +7,11 @@ def f_tau(N, S):
 
 n = 100
 N = np.linspace(1, 10, num = n)
-S = 1/2#1/math.sqrt(2)
+S = 1/2#math.sqrt(2)
 
 
 Ntrans = (2*math.pi*S)/math.acos(1-2*(S**2))
+print('Ntrans = ', Ntrans)
 at = np.zeros(n)
 
 Vp = np.zeros(n)
@@ -25,9 +26,25 @@ for i in range(n):
         at[i] = -math.log(-tau-math.sqrt((tau**2)-1))
     Vp[i] = (2*math.pi)/(N[i]*k)
 
+# fig 2.1
+
 fig, (ax1, ax2) = plt.subplots(2)
-#Plotting current
-line1,    = ax1.plot(N, Vp, color='r', label='')
+
+ax1.plot(N, Vp,'r', label = 'Numerical phase velocity')
+ax3 = ax1.twinx()
+ax3.plot(N, at, 'b--', label = 'Attenuation constant')
+
+ax1.set_ylabel('Numerical Phase Velocity (nomalized to c)')
+ax1.legend()
+
+ax3.set_ylabel('Attenuation Constant (nepers/grid cell)')
+ax3.set_ylim(0, 6)
+ax3.legend()
+
+ax1.axvline(x = Ntrans, color = 'g', linestyle = '--')
+
+
+
 
 N = np.array(range(3,80))
 Vp = np.zeros(np.size(N))
@@ -38,7 +55,8 @@ for i in range(np.size(N)):
 
 Vp = np.log10(100*(1-Vp))
 
-line2,    = ax2.plot(N, Vp, color='b', label='Voltage (v) [V]')
+# fig 2.2
 
-#plt.plot(N, Vp, N, at)
+ax2.plot(N, Vp, color='b')
+
 plt.show()
--- FDTD-wave/wave-FDTD.py
+++ FDTD-wave/wave-FDTD.py
@@ -6,36 +6,83 @@ um = lambda t: 1 if t >= 0 else 0
 v = lambda n, S: um(n) - um(n-(40/S))
 gauss = lambda n, S: math.exp(-((n*S-40)**2)/(2*(10**2)))
 
-
 def FDTD(S, I, N, s):
     u = np.zeros((N,I))
 
-    u[0, 0] = s(0, S)
+    u[0, 0] = s(0, S(0))
     for n in range(1, N):
-        u[n, 0] = s(n, S)
+        u[n, 0] = s(n, S(0))
         for i in range(1, I-1):
-            u[n, i] = (S**2)*(u[n-1, i+1] - 2*u[n-1, i] + u[n-1, i-1]) + 2*u[n-1, i] - u[n-2, i]
+            u[n, i] = (S(i)**2)*(u[n-1, i+1] - 2*u[n-1, i] + u[n-1, i-1]) + 2*u[n-1, i] - u[n-2, i]
 
     return u
 
+def fig_2_3_4(S0, s):
+    S = lambda i: S0
+    I = 200
+    N = int(I/S0)
 
+    u = FDTD(S, I, N, s)
 
-S = 1
-I = 200
-N = 200
+    label = 'S = ' + str(S0)
+    plt.plot(np.array(range(I)), u[int(150/S0)], label = label)
+    plt.legend()
 
-s = gauss
+def fig_2_5():
+    S = lambda i: 1 if i < 140 else 0.25
+    I = 200
+    N = 400
 
-u = FDTD(S, I, N, s)
-label = 'S = ' + str(S)
-plt.plot(np.array(range(I)), u[150], 'r--', label = label)
+    u = FDTD(S, I, N, gauss)
 
-S = 0.5
-N = int(N/S)
+    plt.plot(np.array(range(I)), u[250])
+    plt.axvline(x = 140, color = 'r', linestyle = '--')
 
-u = FDTD(S, I, N, s)
-label = 'S = ' + str(S)
-plt.plot(np.array(range(I)), u[int(150/S)], 'b', label = label)
+def fig_2_6():
+    S = lambda i: 1.0005
+    I = 220
+    N = 400
+    s = gauss
 
-plt.legend()
+    u = FDTD(S, I, N, s)
+
+    fig, (ax1, ax2) = plt.subplots(2)
+
+    # fig 2.6 (a)
+    ax1.plot(np.array(range(I)), u[200], 'r', label = 'n = 200')
+    ax1.plot(np.array(range(I)), u[210], 'b-', label = 'n = 210')
+    ax1.plot(np.array(range(I)), u[220], 'b--', label = 'n = 220')
+    
+    # fig 2.6 (b)
+    ax2.plot(np.array(range(20)), u[200][:20], 'r', label = 'n = 200')
+    ax2.plot(np.array(range(20)), u[210][:20], 'b-', label = 'n = 210')
+    ax2.plot(np.array(range(20)), u[220][:20], 'b--', label = 'n = 220')
+
+    #plt.legend()
+
+def fig_2_7():
+    S = lambda i: 1 if i != 90 else 1.075
+    I = 220
+    N = 300
+    #precisa consertar essa fonte
+    s = lambda n, S: math.exp(-((n*S-60)**2)/(2*(5**2)))
+
+    u = FDTD(S, I, N, s)
+
+    fig, (ax1, ax2) = plt.subplots(2)
+
+    # fig 2.7 (a)
+    ax1.plot(np.array(range(I)), u[190], 'r', label = 'n = 190')
+    ax1.plot(np.array(range(I)), u[200], 'b-', label = 'n = 200')
+    
+    # fig 2.7 (b)
+    ax2.plot(np.array(range(40)) + 70, u[190][70:110], 'r', label = 'n = 200')
+    ax2.plot(np.array(range(40)) + 70, u[200][70:110], 'b-', label = 'n = 210')
+    
+    #plt.legend()
+    
+
+fig_2_7()
+plt.ylabel('Wavefunction u(i)')
+plt.xlabel('Grid i coordinate')
 plt.show()
